0c636f0c43b58417162d39b756384c2e8896a7bc
Juan Bedoya <jbedoya@hexacta.com>
2022-08-28 21:33:29 -0500

    Primer commit prueba1 Bot Framework

---
+++ Prueba1/EmptyBot.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Prueba1
+{
+    public class EmptyBot : ActivityHandler
+    {
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hello world!"), cancellationToken);
+
+                }
+            }
+        }
+        protected override async  Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext,CancellationToken cancellationToken) 
+        {
+            if (string.Equals(turnContext.Activity.Text, "carro", System.StringComparison.InvariantCultureIgnoreCase))
+            {
+                await turnContext.SendActivitiesAsync(
+                    new Activity[] {
+                new Activity { Type = ActivityTypes.Typing },
+                new Activity { Type = "delay", Value= 3000 },
+                MessageFactory.Text("Finished typing", "Finished typing"),
+                    },
+                    cancellationToken);
+            }
+            else
+            {
+                var replyText = $"Echo: {turnContext.Activity.Text}. Say 'wait' to watch me type.";
+                await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            }
+        }
+
+    }
+}
